eded23212be9fdbc1e05c1c6e016cd21ba0847c7
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-06-08 02:35:24 +0300

    Add files via upload

--- podium/script/main.js
+++ podium/script/main.js
@@ -8,6 +8,38 @@ $(document).ready(function () {
         autoplaySpeed: 2000
     });
 
+    $('.goods__list--slick').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false,
+        prevArrow: $('.slider-left'),
+        nextArrow: $('.slider-right'),
+    });
+
     //Маска телефона
     $(".call-me-input").mask("+38(999)999-99-99");
+    $(".ordering__tel").mask("+38 999 999 99 99");
+
+    //Кастомный input number
+    $('.counter .counter__button').on('click', function () {
+        var input = $(this).closest('.counter').find('input'); // инпут
+        var value = parseInt(input.val()) || 0; // получаем value инпута или 0
+        if ($(this).hasClass('minus')) {
+            if (value - 1 > 0)
+                value = value - 1; // вычитаем из value 1
+        }
+        if ($(this).hasClass('plus')) {
+            value = value + 1; // прибавляем к value 1
+        }
+        input.val(value).change(); // выводим полученное value в инпут; триггер .change() - на случай, если на изменение этого инпута у вас уже объявлен еще какой-то обработчик
+        // initCart();
+    });
+
+    $('.counter .counter__input').on('keyup', function () {
+        if (!$(this).val().length) {
+            $(this).val(1);
+        }
+    });
 });
