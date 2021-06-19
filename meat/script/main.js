3e7e85b9a238d1e581f045b48b1aaa70264590c6
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-06-19 03:28:14 +0300

    Add files via upload

--- meat/script/main.js
+++ meat/script/main.js
@@ -274,19 +274,6 @@ function initMap() {
 
 
 $(document).ready(function () {
-
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-    window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-
     //кастомный select
     jQuery(($) => {
         $('.select').on('click', '.select__head', function () {
@@ -564,6 +551,18 @@ $(document).ready(function () {
     window.addEventListener('resize', function (event) {
         moving_header_elements();
     }, true);
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
     // Открытие и закрытие мобильлного меню
     function menu_open() {
