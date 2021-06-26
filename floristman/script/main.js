35938ef57bc6e535e44366fd564e89bf51cbde72
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-06-26 04:50:47 +0300

    Add files via upload

---
+++ floristman/script/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    $('.products__list--scroll').slick({
+        dots: true,
+        dotsClass: "my-dots",
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+});
