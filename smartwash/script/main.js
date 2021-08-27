509005834cafa8ce0e5efcbf60dd94a5918297fc
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-08-28 02:20:35 +0300

    Add files via upload

--- smartwash/script/main.js
+++ smartwash/script/main.js
@@ -3,5 +3,40 @@ $(document).ready(function () {
     //Маска телефона
     $(".phone_mask").mask("+7(999)999-99-99");
 
+    $('.why-us__list').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        adaptiveHeight: true,
+    });
+
+    $('.facilities__list').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        adaptiveHeight: true,
+    });
+
+    $('.visual__sliders').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        adaptiveHeight: true,
+    });
+
+
+    $('.reasons__list').slick({
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: false,
+        dots: true,
+        infinite: false,
+    });
 
 });
