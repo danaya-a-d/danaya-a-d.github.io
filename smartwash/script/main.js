92506972b192ce6a33c5bcdf99db1594e26bf96d
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-08-29 04:24:11 +0300

    Add files via upload

--- smartwash/script/main.js
+++ smartwash/script/main.js
@@ -1,189 +1,220 @@
-//Показ слайдеров на мобильном
-function slick_mobile(slider, settings) {
-    // Подпишемся на ресайз и продиспатчим его для запуска
-    $(window).on('resize', function (e) {
-        // Переменная, по которой узнаем запущен слайдер или нет.
-        // Храним её в data
-        let init = slider.data('init-slider');
-        // Если мобильный
-        if (window.innerWidth < 769) {
-            // Если слайдер не запущен
-            if (init !== 1) {
-                // Запускаем слайдер и записываем в data init-slider = 1
-                slider.slick(settings).data({'init-slider': 1});
-            }
-        }
-        // Если десктоп
-        else {
-            // Если слайдер запущен
-            if (init === 1) {
-                // Разрушаем слайдер и записываем в data init-slider = 0
-                slider.slick('unslick').data({'init-slider': 0});
-            }
-        }
-    }).trigger('resize');
-}
-
 $(document).ready(function () {
 
     //Маска телефона
     $(".phone_mask").mask("+7(999)999-99-99");
 
-    //Слайдер "Кому может быть полезна остеопатия?"
-    $('.useful__list').slick({
-        dots: true,
-        arrows: false
-    });
-
-    //Слайдер отзывов
-    $('.reviews__list').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 4,
+    $('.why-us__list').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        rows: 1,
+        infinite: false,
+        adaptiveHeight: true,
         responsive: [
             {
-                breakpoint: 769,
+                breakpoint: 991,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    rows: 2
+                    dots: true,
+                    arrows: false,
                 }
             }
         ]
     });
 
-    //Слайдер методов лечения
-    const settings = {
+    $('.facilities__list').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('.visual__sliders').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        adaptiveHeight: true
+    });
+
+    $('.reasons__list').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        variableWidth: true,
         dots: true,
         arrows: false,
-        mobileFirst: true,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    $('.reviews__list').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
         adaptiveHeight: true,
-        settings: 'slick',
-        responsive: [{
-            breakpoint: 769,
-            settings: 'unslick'
-        }]
-    };
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
-    slick_mobile($('.methods__list'), settings);
-    slick_mobile($('.licenses__photos'), settings);
-    slick_mobile($('.stocks__list'), settings);
+    $('.professionals__list').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+        adaptiveHeight: true,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
 
+
+    $('.fixed-video__close').on('click', function (e) {
+        $('.fixed-video').hide();
+    });
 
     //Модальные окна
+    function show(modal) {
+        modal.removeClass('hide');
+        modal.addClass('show');
+        $('.modal__overlay').removeClass('hide');
+        $('.modal-back').removeClass('hide');
+    }
+
+    function hide(modal) {
+        modal.addClass('hide');
+        $('.modal__overlay').addClass('hide');
+        $('.modal-back').addClass('hide');
+    }
+
     function modal_show(modal, button_open) {
 
-        function show() {
-            modal.removeClass('hide');
-            modal.addClass('show');
-            $('.modal__overlay').removeClass('hide');
-            $('.modal-back').removeClass('hide');
+        if (button_open !== null) {
+            button_open.on('click', function (e) {
+                show(modal);
+            });
         }
 
-        function hide() {
-            modal.addClass('hide');
-            $('.modal__overlay').addClass('hide');
-            $('.modal-back').addClass('hide');
-        }
-
-        button_open.on('click', function (e) {
-            show();
-        });
         $('.modal-close').on('click', function (e) {
-            hide();
+            hide(modal);
         });
+
         $('.modal__overlay').on('click', function (e) {
             modal.addClass('hide');
-            hide();
+            hide(modal);
         });
+
         $(document).keydown(function (e) {
             if (e.which === 27) {
-                hide();
+                hide(modal);
             }
         });
     }
 
-    modal_show($('.modal'), $('.recall-js'));
+    modal_show($('.modal--recall'), $('.recall-js'));
+    modal_show($('.modal--calc'), $('.calc-js'));
+    modal_show($('.modal--table'), $('.table-js'));
+    modal_show($('.modal--step-1'), $('.request-js'));
 
-    //Плавное пролистывание к якорю
-    $('a[href*="#"]').click(function (e) {
-
-        $('html, body').stop().animate({
-            scrollTop: $(this.hash).offset().top
-        }, 1000);
+    $('.modal--step-1 .button').on('click', function (e) {
         e.preventDefault();
-    })
+        hide($('.modal--step-1'));
+        show($('.modal--step-2'));
+        modal_show($('.modal--step-2'), null)
+    });
 
-    //Отправка формы
-    $('.form__button').click(function (e) {
-        let form_data = $(this).closest('.form').serializeArray(); // Собираем все данные из формы
-        let valid = true;
+    $('.modal--step-2 .button').on('click', function (e) {
+        e.preventDefault();
+        hide($('.modal--step-2'));
+        show($('.modal--step-3'));
+        modal_show($('.modal--step-3'), null)
+    });
 
-        $(this).closest('.form').find($('.form__input')).each(function () {
-            let value;
-            if ($(this).val() !== '') {
-                value = 1;
-            } else value = 0;
-            valid *= value;
-        });
+    $('.modal--step-3 .button').on('click', function (e) {
+        e.preventDefault();
+        hide($('.modal--step-3'));
+        show($('.modal--step-4'));
+        modal_show($('.modal--step-4'), null)
+    });
 
-        if (valid) {
-            e.preventDefault();
-            $('body').addClass('sending');
 
-            $.ajax({
-                type: "POST", // Метод отправки
-                url: "../action.php", // Путь до php файла отправителя
-                data: form_data,
-                success: function () {
-                    // Код в этом блоке выполняется при успешной отправке сообщения
-                    $('body').removeClass('sending');
-                    alert("Ваше сообщение отправлено!");
-                },
-                error: function () {
-                    // Код в этом блоке выполняется при неуспешной отправке сообщения
-                    $('body').removeClass('sending');
-                    alert("Сообщение не отправлено!");
-                }
-            });
+    $('.recall-close').on('click', function (e) {
+        hide($('.modal--table'));
+        show($('.modal--recall'));
+        modal_show($('.modal--recall'), null)
+    });
+
+
+    let first = true;
+    $(document).mouseleave(function () {
+        if (first && $('.modal-back').hasClass('hide')) {
+            show($('.modal--wait'));
+            modal_show($('.modal--wait'), null)
+            first = false;
         }
     });
 
 
-    //Открытие FAQ
-    let faq_item = document.querySelectorAll('.faq__item');
+    let range_auto = document.getElementById('range-auto');
+    let range_check = document.getElementById('range-check');
+    let range_chem = document.getElementById('range-chem');
+    let range_rent = document.getElementById('range-rent');
+    let range_water = document.getElementById('range-water');
 
-    if (faq_item !== undefined) {
-
-        for (let i = 0; i < faq_item.length; i++) {
-            let faq_text = faq_item[i].querySelector('.faq__text');
-
-            faq_item[i].addEventListener('click', function (event) {
-
-                if (faq_text.classList.contains('hidden')) {
-
-                    for (let j = 0; j < faq_item.length; j++) {
-
-                        let faq_text = faq_item[j].querySelector('.faq__text');
-                        faq_text.classList.add('hidden');
-                        faq_item[j].classList.remove('faq__item--active');
-                    }
-
-                }
-
-                faq_text.classList.toggle('hidden');
-                faq_item[i].classList.toggle('faq__item--active');
-
-                if (faq_item[i].classList.contains('faq__item--active')) {
-                    if (window.matchMedia('(max-width: 769px)').matches) {
-                        $('html, body').animate({
-                            scrollTop: $(this).offset().top - 60
-                        }, 300);
-                    }
-                }
-            });
+    const setting_range = {
+        start: 850,
+        range: {
+            'min': 500,
+            'max': 3500
+        },
+        connect: [true, false],
+        // Show a scale with the slider
+        pips: {
+            mode: 'steps',
+            stepped: true,
+            density: 4
         }
     }
+
+    noUiSlider.create(range_auto, setting_range);
+    noUiSlider.create(range_check, setting_range);
+    noUiSlider.create(range_chem, setting_range);
+    noUiSlider.create(range_rent, setting_range);
+    noUiSlider.create(range_water, setting_range);
+
 });
