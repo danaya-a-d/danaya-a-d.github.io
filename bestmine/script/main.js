0da1739c0bb75fe8175e02b89434b32e122e37dc
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-09-25 01:12:40 +0300

    Add files via upload

--- bestmine/script/main.js
+++ bestmine/script/main.js
@@ -63,4 +63,35 @@ $(document).ready(function () {
     });
 
 
+    //кастомный select
+    jQuery(($) => {
+        $('.select').on('click', '.select__head', function () {
+            if ($(this).hasClass('open')) {
+                $(this).removeClass('open');
+                $(this).next().fadeOut();
+            } else {
+                $('.select__head').removeClass('open');
+                $('.select__list').fadeOut();
+                $(this).addClass('open');
+                $(this).next().fadeIn();
+            }
+        });
+
+        $('.select').on('click', '.select__item', function () {
+            $('.select__head').removeClass('open');
+            $(this).parent().fadeOut();
+            $(this).parent().prev().text('');
+            $(this).parent().prev().append($(this).html());
+            $(this).parent().prev().prev().val($(this).text());
+        });
+
+        $(document).click(function (e) {
+            if (!$(e.target).closest('.select').length) {
+                $('.select__head').removeClass('open');
+                $('.select__list').fadeOut();
+            }
+        });
+    });
+
+
 });
