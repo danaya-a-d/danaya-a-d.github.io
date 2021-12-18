c675edd91b8a900959d2e46578cd018c0d116e89
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2021-12-18 19:18:57 +0300

    Add files via upload

---
+++ training/error-de-pago/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Adn training</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../css/fonts.css" rel="stylesheet" />
+    <link href="../css/not-found.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="../img/favicon.png">
+</head>
+<body>
+
+    <section class="not-found">
+        <div class="container">
+            <div class="not-found-box">
+                <div class="not-found-content">
+                    <h3 class="not-found-title">
+                        Vaya, parece que algo no ha funcionado bien.
+                        No pasa nada.
+                    </h3>
+                    <p>Por favor, comprueba que tu tarjeta de crédito no esté caducada y vuelve a intentarlo.</p>
+                    <a href="#" class="btn-arrow">
+                        <span>PRUEBA OTRA VEZ</span>
+                        <img src="../img/arrow-black.svg" alt="Alternate Text">
+                    </a>
+                </div>
+            </div>
+            <div class="not-found-wrap">
+                <div class="not-found-block">
+                    <div class="not-found-icon"><img src="../img/white-icon-1.svg" alt="Alternate Text" /></div>
+                    <p>
+                        Si por alguna razón no puedes realizar el pago, escríbenos en este <a href="#">chat de whatsapp</a> de whatsapp o contacta con
+                        nuestro departamento del servicio de atención al cliente por correo electrónico: <a href="#">soporte@rimagency.com</a>
+                    </p>
+                </div>
+                <div class="not-found-block">
+                    <div class="not-found-icon"><img src="../img/white-icon-2.svg" alt="Alternate Text" /></div>
+                    <p>
+                        En ningún caso daremos a terceros tu información personal. La seguridad de todos los pagos está garantizada por los certificados THAWTE y PCI.
+                    </p>
+                </div>
+            </div>
+            <div class="center">
+                <img src="../img/logo.svg" alt="Alternate Text" />
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
