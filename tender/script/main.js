305d4a71cb611bf9567f7daad39d878828b27e81
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2022-05-16 00:45:43 +0300

    Add files via upload

--- tender/script/main.js
+++ tender/script/main.js
@@ -1,14 +1,24 @@
 window.addEventListener('DOMContentLoaded', function (event) {
 
 
-    // кастомный input range
     let filter_slider = document.getElementById('filter-slider-js');
     let filter_reset = document.getElementById('filter-reset-js');
     let filter_form = document.getElementById('filter-form-js');
+
     let card_text_block = document.querySelectorAll('.card__text-block');
     let footer_collapse_btn = document.querySelectorAll('.footer-collapse-btn');
     let checkout_collapse_btn = document.querySelectorAll('.checkout-collapse-btn');
 
+    let menu_open_btn = document.querySelector('.header__menu-open-btn');
+    let menu_close_btn = document.querySelector('.header__menu-close-btn');
+    let menu_overlay = document.querySelector('.header__mobile-menu-overlay');
+    let menu_mobile = document.querySelector('.header__mobile-menu ');
+
+    const lightbox = GLightbox({
+        touchNavigation: true,
+        loop: true,
+        autoplayVideos: true
+    });
 
     if (filter_slider !== null) {
         noUiSlider.create(filter_slider, {
@@ -102,8 +112,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
             history_nav.addEventListener('click', function (event) {
                 if (!history_item_content[i].classList.contains('active')) {
                     history_item_content[i].classList.add('active');
-                }
-                else if (history_item_content[i].classList.contains('active')) {
+                } else if (history_item_content[i].classList.contains('active')) {
                     history_item_content[i].classList.remove('active');
                 }
             });
@@ -122,12 +131,83 @@ window.addEventListener('DOMContentLoaded', function (event) {
         }
     }
 
-    // фильтр категорий
 
+    // фильтр категорий
     document.querySelector('body').addEventListener('click', (e) => {
         const target = e.target;
         if (target.classList.contains('filters-btn-js')) {
             filter_form.classList.toggle('open');
         }
     });
+
+
+    // открытие/закрытие мобильного меню
+    menu_open_btn.addEventListener('click', function (event) {
+        menu_mobile.classList.remove('close');
+    });
+
+    menu_overlay.addEventListener('click', function (event) {
+        menu_mobile.classList.add('close');
+    });
+
+    menu_overlay.addEventListener('touchmove', function (event) {
+        menu_mobile.classList.add('close');
+    });
+
+    menu_close_btn.addEventListener('click', function (event) {
+        menu_mobile.classList.add('close');
+    });
+
+
+    // открытие подменю
+    let drop_downs = document.querySelectorAll('.drop-down')
+
+    let drop_down_languages = document.querySelectorAll('.drop-down-languages')
+    let drop_down_languages_btn = document.querySelectorAll('.drop-down-languages-btn')
+
+    let drop_down_login = document.querySelectorAll('.drop-down-login')
+    let drop_down_login_btn = document.querySelectorAll('.drop-down-login-btn')
+
+    let drop_down_registration = document.querySelectorAll('.drop-down-registration')
+    let drop_down_registration_btn = document.querySelectorAll('.drop-down-registration-btn')
+
+    let desktop_search = document.querySelectorAll('.desktop-search')
+    let desktop_search_btn = document.querySelectorAll('.desktop-search-btn')
+
+    let modal_cart = document.querySelectorAll('.modal-back')
+    let modal_cart_btn = document.querySelectorAll('.modal-cart-btn')
+
+    drop_down(drop_down_languages, drop_down_languages_btn);
+    drop_down(drop_down_login, drop_down_login_btn);
+    drop_down(drop_down_registration, drop_down_registration_btn);
+    drop_down(desktop_search, desktop_search_btn);
+    drop_down(modal_cart, modal_cart_btn);
+
+    document.querySelector('body').addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains('drop-down-close-btn')) {
+
+            menu_mobile.classList.add('close');
+
+            for (let i = 0; i < drop_downs.length; i++) {
+                drop_downs[i].classList.add('close');
+            }
+        }
+    });
+
+    function drop_down(block, button) {
+        for (let i = 0; i < button.length; i++) {
+            button[i].addEventListener('click', function () {
+                for (let i = 0; i < drop_downs.length; i++) {
+                    drop_downs[i].classList.add('close');
+                }
+
+                for (let i = 0; i < block.length; i++) {
+                    block[i].classList.toggle('close');
+                }
+            });
+        }
+    }
+
+
 });
