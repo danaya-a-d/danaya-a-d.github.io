48e137f403f91a558fcb631ed7dddaf4cc6b6545
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2022-07-06 17:06:39 +0300

    Add files via upload

--- school977-dashboard/script/main.js
+++ school977-dashboard/script/main.js
@@ -29,6 +29,16 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
+    // Сворачивание вкладок успеваимости
+    $('.performance__item').on('click', function () {
+        $(this).toggleClass('active');
+    });
+
+    // Сворачивание активных заданий
+    $('.task').on('click', function () {
+        $(this).toggleClass('active');
+    });
+
     // Открытие/закрытие меню
     $('.header__menu-btn').on('click', function () {
         $(this).toggleClass('close');
@@ -36,37 +46,59 @@ $(document).ready(function () {
     });
 
     $('.menu__overlay').on('click', function () {
-        $('.header__menu-btn').toggleClass('close');
-        $('.menu').toggleClass('close');
+        $('.header__menu-btn').addClass('close');
+        $('.menu').addClass('close');
     });
 
     $('.menu__overlay').on('touchmove', function () {
-        $('.header__menu-btn').toggleClass('close');
-        $('.menu').toggleClass('close');
+        $('.header__menu-btn').addClass('close');
+        $('.menu').addClass('close');
     });
 
     // диаграммы
 
     let progress_data = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
-
+    let text_color = 'white'
     let progress_statistic = $('.statistic')
 
     for (let i = 0; i < progress_data.length; i++) {
+
+        if (progress_data[i] <= 29) {
+            text_color = '#222529'
+        }
+
         $('.statistic__list').append(
             '<div class="statistic__item">\n' +
             '<div class="statistic__value-block">\n' +
-            '<div class="statistic__value-color"></div>\n' +
-            '<span class="statistic__value-name">Алгебра</span>\n' +
+            '<div class="statistic__value-color" style="height: ' + progress_data[i] + '%"></div>\n' +
+            '<span class="statistic__value-name" style="color: ' + text_color + '">Алгебра</span>\n' +
             '</div>\n' +
-            '<div class="statistic__value-number">100%</div>\n' +
+            '<div class="statistic__value-number">' + progress_data[i] + '%</div>\n' +
             '</div>');
     }
 
-    // var divs = body.selectAll("div")
-    //     .data(data)
-    //     .enter().append("div");
-    //
-    // divs.style("width", function(d) { return d + "px"; })
-    //     .attr("class", "divchart")
-    //     .text(function(d) { return d; });
+    $('.statistic__sort-button').on('click', function () {
+        $('.statistic__list').toggleClass('reverse');
+        $('.statistic__sort-button').toggleClass('reverse');
+    });
+
+
+    // переключение вкладок
+
+    let toggles_items = document.querySelectorAll('.toggles__item');
+    let diary_blocks = document.querySelectorAll('.diary__block');
+
+    for (let i = 0; i < toggles_items.length; i++) {
+
+        toggles_items[i].addEventListener('click', function () {
+
+            for (let j = 0; j < toggles_items.length; j++) {
+                toggles_items[j].classList.remove('active');
+                diary_blocks[j].classList.remove('active');
+            }
+
+            toggles_items[i].classList.add('active');
+            diary_blocks[i].classList.add('active');
+        });
+    }
 });
