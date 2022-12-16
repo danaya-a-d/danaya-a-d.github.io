350a6db0385f0f981fc89eeabe0720697ade8ea0
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2022-12-16 14:47:25 +0300

    Add files via upload

--- dealnox/script/main.js
+++ dealnox/script/main.js
@@ -1,7 +1,18 @@
 window.addEventListener('DOMContentLoaded', function (event) {
 
-    // Slider
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    // Slider
     const reviews_right = new Swiper('.reviews-right', {
         slidesPerView: 'auto',
         speed: 25000,
@@ -54,4 +65,36 @@ window.addEventListener('DOMContentLoaded', function (event) {
     //     reviews_left.autoplay.start();
     //     reviews_left.speed(15000);
     // });
+
+// Mobile menu
+    let header_menu_btn = document.querySelector('.header__menu-btn');
+    let header_nav_menu = document.querySelector('.header__nav');
+    let header_overlay = document.querySelector('.header__overlay');
+
+
+    function menu_open() {
+        if (header_nav_menu.classList.contains('open')) {
+            header_nav_menu.classList.remove('open');
+            header_menu_btn.classList.remove('open');
+            header_overlay.classList.remove('open');
+        } else {
+            header_nav_menu.classList.add('open');
+            header_menu_btn.classList.add('open');
+            header_overlay.classList.add('open');
+        }
+    }
+
+    header_menu_btn.addEventListener('click', function () {
+        menu_open();
+    });
+
+    header_overlay.addEventListener('click', function () {
+        menu_open();
+    });
+
+    header_overlay.addEventListener('touchmove', function () {
+        menu_open();
+    });
+
+
 });
