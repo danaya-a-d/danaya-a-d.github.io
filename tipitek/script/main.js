eb378dd8c019ae494d33f9dad3ed0bc1cda0b2bf
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2023-03-30 17:08:18 +0300

    Add files via upload

--- tipitek/script/main.js
+++ tipitek/script/main.js
@@ -366,12 +366,15 @@ window.addEventListener('DOMContentLoaded', function (event) {
             anchor.addEventListener('click', function (e) {
                 e.preventDefault()
 
+
                 const blockID = anchor.getAttribute('href').substr(1)
 
-                document.getElementById(blockID).scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                })
+                if (blockID !== '') {
+                    document.getElementById(blockID).scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    })
+                }
             })
         }
     }
