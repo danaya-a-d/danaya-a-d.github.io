1e3af8e09cb6a69b513addd22d0ea27e0a25eaf0
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2024-01-05 23:05:15 +0300

    Add files via upload

--- gmt-solution/script/main.js
+++ gmt-solution/script/main.js
@@ -1,3 +1,296 @@
+// Гугл карта
+// The location of Uluru
+const uluru = {lat: 52.428834, lng: 13.323385};
+const popupContent = '<p class="popup">94958 Senko Parks Scherian land,\n' +
+    'MN 31814-2712</p>';
+
+function initMap(center) {
+
+    if (center == null) {
+        center = uluru;
+    }
+
+    // The map, centered at Uluru
+    const mapOptions =  {
+        zoom: 12,
+        fullscreenControl: false,
+        center: center,
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoomControl: false,
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "saturation": "2"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "saturation": "-28"
+                    },
+                    {
+                        "lightness": "-10"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#444444"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#f2f2f2"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "saturation": "-1"
+                    },
+                    {
+                        "lightness": "-12"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "lightness": "-31"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "lightness": "-74"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "lightness": "65"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.landcover",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": "-15"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.landcover",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "lightness": "0"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "saturation": "0"
+                    },
+                    {
+                        "lightness": "-9"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "lightness": "-14"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "lightness": "-35"
+                    },
+                    {
+                        "gamma": "1"
+                    },
+                    {
+                        "weight": "1.39"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "lightness": "-19"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "lightness": "46"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "lightness": "-13"
+                    },
+                    {
+                        "weight": "1.23"
+                    },
+                    {
+                        "invert_lightness": true
+                    },
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#adadad"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            }
+        ]
+    };
+
+    const mapCanvas = document.getElementById("google-map");
+
+    const map = new google.maps.Map(mapCanvas, mapOptions);
+
+    const icon = {
+        url: "img/map-marker.svg", // url
+        scaledSize: new google.maps.Size(66, 81), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        icon: icon,
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+        content: popupContent,
+        pixelOffset: new google.maps.Size(200, 100)
+    });
+
+    infowindow.open(map, marker);
+}
+
+
+
 window.addEventListener('DOMContentLoaded', function (event) {
     //слайдеры
     const first_screen_swiper = new Swiper('.first-screen__sliders', {
