78611ee7c67549d12c6f1427d259bf3989ee322c
danaya-a-d <35764553+danaya-a-d@users.noreply.github.com>
2024-01-16 18:44:40 +0300

    Add files via upload

--- gmt-solution/script/main.js
+++ gmt-solution/script/main.js
@@ -291,6 +291,19 @@ function initMap(center) {
 
 
 window.addEventListener('DOMContentLoaded', function (event) {
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
     //слайдеры
     const first_screen_swiper = new Swiper('.first-screen__sliders', {
         direction: 'horizontal',
@@ -438,4 +451,45 @@ window.addEventListener('DOMContentLoaded', function (event) {
             horizontalOrder: true
         });
     }
+
+    //mobile menu
+    let header_menu_btn = document.querySelector('.header__menu-btn');
+    let header_nav_menu = document.querySelector('.header__nav');
+
+    function menu_open() {
+        if (header_nav_menu.classList.contains('open')) {
+            header_nav_menu.classList.remove('open');
+            header_menu_btn.classList.remove('open');
+        } else {
+            header_nav_menu.classList.add('open');
+            header_menu_btn.classList.add('open');
+        }
+    }
+
+    header_menu_btn.addEventListener('click', function () {
+        menu_open();
+    });
+
+    let header_nav_items = document.querySelectorAll('.main-nav__item');
+
+    for (let i = 0; i < header_nav_items.length; i++) {
+        header_nav_items[i].addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    }
+
+    //обучение
+    let nav_btn = document.querySelector('.nav-btn');
+
+    nav_btn.addEventListener('click', function () {
+        nav_btn.classList.toggle('open');
+    });
+
+    document.addEventListener( 'click', (e) => {
+        const withinBoundaries = e.composedPath().includes(nav_btn);
+
+        if (!withinBoundaries) {
+            nav_btn.classList.remove('open');
+        }
+    })
 });
